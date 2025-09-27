1841f068fda14aab0b938bd511e22770facbd1f7
VictorDemerji <demerji.v1@gmail.com>
2025-09-27 13:09:47 +0300

    Add Ex2

---
+++ src/Ex2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introdu un numar: ");
+        int numar1 = sc.nextInt();
+        System.out.print("Introdu alt numar: ");
+        int numar2 = sc.nextInt();
+        System.out.println(numar1*numar2);
+
+    }
+}
