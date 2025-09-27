be990f20379bac82767a79d79986cf283c4ccbce
VictorDemerji <demerji.v1@gmail.com>
2025-09-27 13:45:59 +0300

    Add Ex11

---
+++ src/Ex11.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ex11 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Introdu un text.");
+        String text = sc.nextLine();
+        System.out.println("Indicele maxim este: " + (text.length()-1));
+        System.out.println("Marimea sirului este: " + text.length());
+
+    }
+}
