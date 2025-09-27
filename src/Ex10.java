e577ffd149bfe9fade7dd4c1bcccc50e7676048a
VictorDemerji <demerji.v1@gmail.com>
2025-09-27 13:42:45 +0300

    Add Ex10

---
+++ src/Ex10.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Ex10 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Introdu un text.");
+        String text = sc.nextLine();
+        System.out.println(text.toUpperCase());
+    }
+}
