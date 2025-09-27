db41570be48cc49209722bbe1d90d3d70cc5d0a9
VictorDemerji <demerji.v1@gmail.com>
2025-09-27 13:53:07 +0300

    Add Ex13

---
+++ src/Ex13.java
@@ -0,0 +1,8 @@
+public class Ex13 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 20; i++) {
+            if (i == 14) break;
+            System.out.println(i);
+        }
+    }
+}
