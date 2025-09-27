ade51f81bb99c29fef2f3f2ecd6c3b96f9abf5c9
VictorDemerji <demerji.v1@gmail.com>
2025-09-27 14:05:45 +0300

    Add Ex15

---
+++ src/Ex15.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Ex15 {
+    public static void main(String[] args) {
+        byte [] sir = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(sir));
+        sir[3] = 10;
+        System.out.println(Arrays.toString(sir));
+
+    }
+}
