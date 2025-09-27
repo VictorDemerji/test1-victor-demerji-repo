68823fee797ccb33175539b13ec4e9f1ffe3f36d
VictorDemerji <demerji.v1@gmail.com>
2025-09-27 14:17:55 +0300

    Add Ex17

---
+++ src/Ex17.java
@@ -0,0 +1,15 @@
+public class Ex17 {
+    public static void main(String[] args) {
+        int[] sir = {15, 7, 69, 3, 42};
+
+        int max = sir[0];
+        for (int i = 1; i < sir.length; i++) {
+            if (sir[i] > max) {
+                max = sir[i];
+            }
+        }
+
+        System.out.println("Cel mai mare element este: " + max);
+    }
+}
+
