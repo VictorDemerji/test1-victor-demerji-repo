7c0b566c2034805850b31b105482e2979a9fdb38
VictorDemerji <demerji.v1@gmail.com>
2025-09-27 13:12:55 +0300

    Add Ex3

--- src/Ex3.java
+++ src/Ex3.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 
-public class Ex2 {
+public class Ex3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Introdu un numar: ");
         int numar1 = sc.nextInt();
         System.out.print("Introdu alt numar: ");
         int numar2 = sc.nextInt();
+        System.out.println(numar1+numar2);
+        System.out.println(numar1-numar2);
         System.out.println(numar1*numar2);
+        System.out.println(numar1/numar2);
     }
 }
