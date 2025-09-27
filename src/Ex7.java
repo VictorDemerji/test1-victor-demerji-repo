63ca830a5db0ed5e4afc77c44bf1352e6de9e467
VictorDemerji <demerji.v1@gmail.com>
2025-09-27 13:27:26 +0300

    Add Ex7

---
+++ src/Ex7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introdu un numar: ");
+        int numar = sc.nextInt();
+        if ((numar%2) == 0) {
+            System.out.println("Numarul " + numar + " este par.");
+        } else {
+            System.out.println("Numarul " + numar + " este impar.");
+        }
+    }
+}
